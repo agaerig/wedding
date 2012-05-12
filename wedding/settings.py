5a064d69af805a3ac135508c6878cd773c83a39c
agaerig <agaerig@gmail.com>
2012-05-12 17:01:46 -0500

    added boto to settings file

--- wedding/settings.py
+++ wedding/settings.py
@@ -49,12 +49,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'uploads')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/uploads/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -65,7 +65,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, '../public/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://cdn.drewnora.s3.amazonaws.com/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -150,3 +150,18 @@ LOGGING = {
         },
     }
 }
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_ACCESS_KEY_ID = 'AKIAILINAXGPFPYACBRQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cdn.drewnora'
+AWS_S3_SECURE_URLS = False
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = True
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+try:
+    from local_settings import *
+except:
+    pass
+

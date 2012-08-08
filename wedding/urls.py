88859e9a10f55c5d1152512ae25ca41ba4ab1f94
agaerig <agaerig@gmail.com>
2012-08-08 02:03:00 -0500

    version 2.0 of the website going up post-invitations

--- wedding/urls.py
+++ wedding/urls.py
@@ -1,20 +1,21 @@
 import os
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'drewnora.views.home', name='home'),
-    url(r'^rsvp/$', 'drewnora.views.rsvp', name='rsvp'),
-    url(r'^information/$', 'drewnora.views.information', name='information'),
-    url(r'^annora-and-drew/$', 'drewnora.views.couple', name='couple'),
-    url(r'^detroit/$', 'drewnora.views.detroit', name='detroit'),
-    url(r'^registry/$', 'drewnora.views.registry', name='registry'),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^$', direct_to_template, {'template': 'wedding/home.html'}, name='home'),
+    url(r'^information/$', direct_to_template, {'template': 'wedding/interior/information.html'}, name='information'),
+    url(r'^about-us/$', direct_to_template, {'template': 'wedding/interior/about-us.html'}, name='about-us'),
+    url(r'^accommodations/$', direct_to_template, {'template': 'wedding/interior/accommodations.html'}, name='accommodations'),
+    url(r'^detroit/$', direct_to_template, {'template': 'wedding/interior/detroit.html'}, name='detroit'),
+    url(r'^registry/$', direct_to_template, {'template': 'wedding/interior/registry.html'}, name='registry'),
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+    #     {'document_root': settings.MEDIA_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
